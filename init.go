71749d2f4939afd1f49963b6c3b7b30c8954c61e
Administrator <douzicao@gmail.com>
2022-01-16 14:26:51 +0800

    douzicao

--- init.go
+++ init.go
@@ -26,4 +26,66 @@ func init() {
 	initLogin()
 	initSubmit()
 	initNotify()
+	buildHttpTransportWithProxy()
+	if Transport != nil {
+		logs.Info("可口的双层之芝士夹心饼。")
+	} else {
+		logs.Info("美味的芝士夹心饼。")
+	}
+	logs.Info(
+		"芝士“"
+	)
+}
+
+var Transport *http.Transport
+
+func buildHttpTransportWithProxy() {
+	addr := jd_cookie.Get("http_proxy")
+	if strings.Contains(addr, "http://") {
+		if addr != "" {
+			u, err := url.Parse(addr)
+			if err != nil {
+				logs.Warn("can't connect to the http proxy:", err)
+				return
+			}
+			Transport = &http.Transport{Proxy: http.ProxyURL(u)}
+		}
+	}
+	if strings.Contains(addr, "sock5://") || strings.Contains(addr, "socks5://") {
+		addr = strings.Replace(addr, "sock5://", "", -1)
+		addr = strings.Replace(addr, "socks5://", "", -1)
+		var auth *proxy.Auth
+		v := strings.Split(addr, "@")
+		if len(v) == 3 {
+			auth = &proxy.Auth{
+				User:     v[1],
+				Password: v[2],
+			}
+			addr = v[0]
+		}
+		dialer, err := proxy.SOCKS5("tcp", addr, auth, proxy.Direct)
+		if err != nil {
+			logs.Warn("can't connect to the sock5 proxy:", err)
+			return
+		}
+		Transport = &http.Transport{
+			Dial: dialer.Dial,
+		}
+	}
+}
+
+func GetEnvs(ql *qinglong.QingLong, s string) ([]qinglong.Env, error) {
+	envs, err := qinglong.GetEnvs(ql, s)
+	if err != nil {
+		if s == "JD_COOKIE" {
+			i := 0
+			for _, env := range envs {
+				if env.Status == 0 {
+					i++
+				}
+			}
+			ql.SetNumber(i)
+		}
+	}
+	return envs, err
 }
